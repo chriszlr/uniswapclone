a63ec93faa1988bcbaaac31c0327296f5d8e5551
Chris <chriszoelln@gmail.com>
2022-11-21 08:11:44 +0100

    bug fix

--- components/HeroSection/HeroSection.jsx
+++ components/HeroSection/HeroSection.jsx
@@ -58,7 +58,7 @@ const HeroSection = ({ accounts, tokenData }) => {
         )}
       </div>
 
-      {openSetting && <Token openSetting={openSetting} />}
+      {openSetting && <Token setOpenSetting={setOpenSetting} />}
 
       {openToken && (
         <SearchToken openToken={setOpenToken} tokens={setTokenOne} tokenData={tokenData} />
